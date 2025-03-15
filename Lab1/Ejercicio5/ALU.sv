cec29a78ef2ecce63808df9910ef7d0c6eb58a90
Oscar David Conejo Canton <oscarconejoc@estudiantec.cr>
2025-03-15 14:28:50 -0600

    ALU
    Falta revisar corrimiento a la izquierda del operando A y corrimiento a la derecha del operando A

---
+++ Lab1/Ejercicio5/ALU.sv
@@ -0,0 +1,71 @@
+module ALU # (parameter Ancho = 8)(
+    input    logic [Ancho-1:0]  A,
+    input    logic [Ancho-1:0]  B,
+    input    logic [3:0]        ALUControl,
+    input    logic              ALUFlagIn,
+    output   logic              ALUFlags,
+    output   logic [Ancho-1:0]  ALUResult,
+    output   logic              Z
+);
+    always_comb begin  
+        case (ALUControl)
+            4'h0: begin //and
+                assign ALUResult = A & B;
+            end
+            4'h1: begin //or
+                assign ALUResult = A | B;
+            end
+            4'h2: begin //suma
+                logic [Ancho:0] temp_sum; 
+                assign temp_sum = A + B;
+                assign ALUResult = temp_sum[Ancho-1:0]; 
+                assign ALUFlags = temp_sum[Ancho]; 
+            end
+            4'h3: begin //incremento
+                if (ALUFlagIn) begin
+                    assign ALUResult = B + 1;
+                end
+                else begin
+                    assign ALUResult = A + 1;
+                end
+            end
+            4'h4: begin //decremento
+                if (ALUFlagIn) begin
+                    assign ALUResult = B - 1;
+                end
+                else begin
+                    assign ALUResult = A - 1;
+                end
+            end
+            4'h5: begin //not
+                if (ALUFlagIn) begin
+                    assign ALUResult = ~B;
+                end
+                else begin
+                    assign ALUResult = ~A;
+                end
+            end
+            4'h6: begin //resta
+                assign {ALUFlags, ALUResult} = A - B;
+            end
+            4'h7: begin //xor
+                assign ALUResult = A ^ B;
+            end
+            4'h8: begin //corriemiento Izquierda
+                assign ALUResult = A << B;
+            end
+            4'h8: begin //corriemiento Derecha
+                assign ALUResult = A >> B;
+            end
+            default: begin
+                assign ALUResult = 4'b1111;
+            end
+        endcase
+        if (ALUResult == 0) begin
+            assign Z = 1;
+        end
+        else assign Z = 0;
+
+    end
+
+endmodule
---
+++ Lab1/Ejercicio5/TBEje5.sv
@@ -0,0 +1,79 @@
+`timescale 1ns/1ps
+
+module TBEje5 ();
+    logic   [3:0]          A;
+    logic   [3:0]          B;
+    logic   [3:0]          ALUControl;
+    logic                  ALUFlagIn;
+    logic                  ALUFlags;
+    logic   [3:0]          ALUResult;
+    logic                  Cero;
+
+
+    ALU #(.Ancho(4)) ALU1(
+        .A              (A),
+        .B              (B),
+        .ALUControl     (ALUControl),
+        .ALUFlagIn      (ALUFlagIn),
+        .ALUFlags       (ALUFlags),
+        .ALUResult      (ALUResult),
+        .Z           (Cero)
+    );
+
+    initial begin
+        A = 4'b0000;
+        B = 4'b0000;
+        ALUControl = 4'h0;
+        ALUFlagIn = 0;
+
+        #50
+        A = 4'b0110;
+        B = 4'b0010;
+
+        #50
+        ALUControl = 4'h1;
+
+        #50
+        ALUControl = 4'h2;
+
+        #50
+        ALUControl = 4'h3;
+        ALUFlagIn = 0;
+
+        #50
+        ALUControl = 4'h3;
+        ALUFlagIn = 1;
+
+        #50
+        ALUControl = 4'h4;
+        ALUFlagIn = 0;
+
+        #50
+        ALUControl = 4'h4;
+        ALUFlagIn = 1;
+
+        #50
+        ALUControl = 4'h5;
+        ALUFlagIn = 0;
+
+        #50
+        ALUControl = 4'h5;
+        ALUFlagIn = 1;
+
+        #50
+        ALUControl = 4'h6;
+
+        #50
+        ALUControl = 4'h7;
+
+        #50
+        ALUControl = 4'h8;
+
+        #50
+        ALUControl = 4'h9;
+
+
+    end
+
+
+endmodule
