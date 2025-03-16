971ea381f4b2fdf198caab5aa9b085d512e795f2
Oscar David Conejo Canton <oscarconejoc@estudiantec.cr>
2025-03-15 19:53:40 -0600

    Documentacion Eje2

--- Lab1/Eje2/Mux.sv
+++ Lab1/Eje2/Mux.sv
@@ -11,35 +11,36 @@ module Mux #(parameter  WIDTH = 8) (
     out
 );
 
-input logic     [WIDTH:0]        in1;
-input logic     [WIDTH:0]        in2;
-input logic     [WIDTH:0]        in3;
-input logic     [WIDTH:0]        in4;
-input logic     [1:0]            sel;
+input  logic     [WIDTH:0]        in1;
+input  logic     [WIDTH:0]        in2;
+input  logic     [WIDTH:0]        in3;
+input  logic     [WIDTH:0]        in4;
+input  logic     [1:0]            sel;
 output logic     [WIDTH:0]        out;
 
 always_comb begin 
     case (sel)
-        2'b00: begin
+        2'b00: begin //Seleccion de entrada 1
             out = in1;
         end 
 
-        2'b01: begin
+        2'b01: begin //Seleccion de entrada 2
             out = in2;
         end 
 
-        2'b10: begin
+        2'b10: begin //Seleccion de entrada 3
             out = in3;
         end 
 
-        2'b11: begin
+        2'b11: begin //Seleccion de entrada 4
             out = in4;
         end 
 
-        default: begin
+        default: begin //Caso default
             out = 1'b0;
         end 
     endcase
 end
     
 endmodule
+
